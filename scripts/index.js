3798729e285861d386fc31e63085cd1010029fde
IndigoSoftwares21 <86530640+IndigoSoftwares21@users.noreply.github.com>
2022-07-11 13:20:26 +0100

    additions

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+let maintext = document.getElementById('maintext');
+window.onload = locate();
+
+function locate()
+{
+    maintext.innerHTML = "Locate"
+    setTimeout(sort, 1000)
+}
+
+
+function sort()
+{
+    maintext.innerHTML = "Sort"
+    setTimeout(organize, 1000)
+}
+
+function organize()
+{
+    maintext.innerHTML = "Organize"
+    setTimeout(locate, 1000)
+}
+
+const download = ()=>{
+    location.href = "download/Loggr Setup.exe"
+}
